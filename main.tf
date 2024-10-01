dc50a089180e0480de6a050f68b260e1103f17f4
Suyash Ahire <66070869+suyash43@users.noreply.github.com>
2024-10-01 15:45:14 +0530

    changed name

--- main.tf
+++ main.tf
@@ -14,7 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-resource "aws_instance" "suyash-blog" {
+resource "aws_instance" "suyash-web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
